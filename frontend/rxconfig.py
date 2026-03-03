c6ecd249352ca9e36269aad8a1c63359cec6350a
Aitor <afarina@eave.es>
2026-03-03 19:39:50 +0000

    connect backend

--- frontend/rxconfig.py
+++ frontend/rxconfig.py
@@ -4,5 +4,5 @@ config = rx.Config(
     app_name="cv_analyzer_ui",
     frontend_port=8080,
     backend_port=8080,
-    
+    api_url="https://cv-analyzer-869270551654.europe-west1.run.app/api/v1/analyze"
 )
